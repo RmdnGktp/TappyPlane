6ec0bb874765593c914378e3f3ac3ed7aad87ebb
Ramadan <ramadanclub@gmail.com>
2026-03-29 11:42:34 +0200

    feat: Add background and ground movement

---
+++ Assets/Scripts/EnviromentScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnviromentScript : MonoBehaviour
+{
+    
+    [SerializeField] PlaneScript planeScript;
+    [SerializeField] float speed;
+    [SerializeField] float width;
+
+    void Update()
+    {
+        if (!planeScript.isAlive) return;
+
+        transform.Translate (Vector2.left * speed * Time.deltaTime);
+        if (transform.position.x < -width)
+        {   
+            transform.position += new Vector3(width, 0f, 0f);
+        }
+
+    }
+}
