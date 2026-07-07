bef131d6b8f94d06b480cfd8f9e7b979b3bb8d01
Ramadan <ramadanclub@gmail.com>
2026-07-07 11:43:00 +0200

    refactor: Improve the Spawn Time for pipes in differant size

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,34 +3,39 @@ using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] objects; // 0: top, 1: bottom, 2: fuel, 3: Bat, 4:Bee, 5:Fly
-    [SerializeField] float minSpawnDelay; // default 1.5f
-    [SerializeField] float yDifference = 1.6f;
-    [SerializeField] float xDifference = 1f;
-    [SerializeField] float fuelMaxXDifference = 2.0f;
-    [SerializeField] float fuelMinXDifference = 1.5f;
+    [SerializeField] GameObject[] singleSpikes;
+    [SerializeField] GameObject[] doubleSpikes;
+    [SerializeField] GameObject[] tripleSpikes;
+    [SerializeField] GameObject[] objects;
+
+
+    [SerializeField] float spawnDelay; 
+    [SerializeField] float yShifting;
+    [SerializeField] float xShifting;
+    [SerializeField] float minGap;
+    [SerializeField] float maxGap;
+
+    private float currentGap;
+    private float xShiftingObjects;
     
-    float currentGap;
-    [SerializeField] float minGap = 4.4f;
-    [SerializeField] float maxGap = 4.3f;
-    [SerializeField] float gapDifference = 0.3f;
     [SerializeField] PlaneScript planeScript;
 
-    float difficulty = 0;
+    private float difficulty = 0;
     [SerializeField] Transform Fragments;
     
 
     public void StartSpawn()
     {
         StartCoroutine(SpawnLoop());
-        SpawnPattern();
+        //SpawnPattern();
+        SpawnObstacle(0);
     }
 
     void Update()
     {
         if (!planeScript.isStarted) return;
 
-        difficulty = Mathf.Clamp01 (Time.time / 15f); 
+        difficulty = Mathf.Clamp01 (Time.time / 30f); 
 
         if (!planeScript.isAlive)
         {
@@ -42,58 +47,110 @@ public class SpawnManager : MonoBehaviour
         }
     }
 
+    // SPAWN PIPES ----------------------------------------------------------------------------------------------------
     IEnumerator SpawnLoop()
     {
         while (planeScript.isAlive)
         {   
-            float delay = Mathf.Lerp(2f, minSpawnDelay, difficulty);
-            // print (delay);
-            yield return new WaitForSeconds(delay);
-            SpawnPattern();
+
+            int value = Random.Range(0, 3);
+
+            switch (value)
+            {
+                case 0:
+                spawnDelay = 2f;
+                break;
+
+                case 1:
+                spawnDelay = 2.5f;
+                break;
+
+                case 2:
+                spawnDelay = 3f;
+                break;
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+            SpawnObstacle(value);
         }
     }
 
     void SpawnPattern()
     {   
-        float rand = Random.value;
+        //currentGap = Mathf.Lerp (minGap, maxGap, difficulty);
+        //SpawnObstacle(); 
+    }
 
-        float easyLevel = Mathf.Lerp(0.6f, 0.2f , difficulty);
-        float mediumLevel = Mathf.Lerp(0.3f, 0.2f , difficulty);
-
+    void SpawnObstacle(int value)
+    {
+        float centerY = Random.Range(-yShifting, yShifting);
         currentGap = Mathf.Lerp (minGap, maxGap, difficulty);
-        
-        if (rand < easyLevel)
+
+        switch (value)
         {
-            SpawnObstacle(currentGap + gapDifference); 
+            case 0:
+            SpawnSingleSpikes(centerY, currentGap);
+            xShiftingObjects = 1.5f;
+            break;
+
+            case 1:
+            SpawnDoubleSpikes(centerY, currentGap);
+            xShiftingObjects = 2.0f;
+            break;
+
+            case 2:
+            SpawnTripleSpikes(centerY, currentGap);
+            xShiftingObjects = 2.5f;
+            break;
         }
-        else if (rand < easyLevel + mediumLevel)
-        {
-            SpawnObstacle(currentGap); 
-        }
-        else
-        {
-            SpawnObstacle(currentGap - gapDifference); 
-        }
+
+        SpawnObjects(centerY);
+    }
+
+    void SpawnSingleSpikes(float centerY, float gapSize)
+    {   
+        float topCenterY = centerY + gapSize;
+        float bottomCenterY = centerY - gapSize;
+        float xShift = Random.Range(-xShifting, xShifting);
+
+        int value = Random.Range (0, singleSpikes.Length);
+        Instantiate(singleSpikes[value], new Vector3(transform.position.x, bottomCenterY, 0), Quaternion.identity, gameObject.transform);
+        GameObject TopSpike = Instantiate(singleSpikes[value], new Vector3(transform.position.x + xShift, topCenterY, 0), Quaternion.Euler(0, 0, 180), gameObject.transform);
+        TopSpike.GetComponent<RockScript>().isRotated = true;
 
     }
 
-    void SpawnObstacle(float gapSize)
-    {
-        // Spawn Rocks -----------------------------------------------------------------------------------------------------------
-        float centerY = Random.Range(-yDifference, yDifference);
-        float topY = centerY + gapSize;
-        float bottomY = centerY - gapSize;
+    void SpawnDoubleSpikes(float centerY, float gapSize)
+    {   
+        float topCenterY = centerY + gapSize;
+        float bottomCenterY = centerY - gapSize;
+        float xShift = Random.Range(-xShifting, xShifting);
 
-        float x = Random.Range(-xDifference, xDifference);
-
-        int value = Random.Range(0, 4);
-        Instantiate(objects[value], new Vector3(transform.position.x, bottomY, 0), Quaternion.identity, gameObject.transform);
-        //value = Random.Range(0, 4);
-        GameObject TopSpike = Instantiate(objects[value], new Vector3(transform.position.x + x, topY, 0), Quaternion.Euler(0, 0, 180), gameObject.transform);
+        int value = Random.Range (0, doubleSpikes.Length);
+        Instantiate(doubleSpikes[value], new Vector3(transform.position.x, bottomCenterY, 0), Quaternion.identity, gameObject.transform);
+        value = Random.Range (0, doubleSpikes.Length);
+        GameObject TopSpike = Instantiate(doubleSpikes[value], new Vector3(transform.position.x + xShift, topCenterY, 0), Quaternion.Euler(0, 0, 180), gameObject.transform);
         TopSpike.GetComponent<RockScript>().isRotated = true;
+    }
+
+     void SpawnTripleSpikes(float centerY, float gapSize)
+    {   
+        float topCenterY = centerY + gapSize;
+        float bottomCenterY = centerY - gapSize;
+        float xShift = Random.Range(-xShifting, xShifting);
+
+        int value = Random.Range (0, tripleSpikes.Length);
+        Instantiate(tripleSpikes[value], new Vector3(transform.position.x, bottomCenterY, 0), Quaternion.identity, gameObject.transform);
+        value = Random.Range (0, tripleSpikes.Length);
+        GameObject TopSpike = Instantiate(tripleSpikes[value], new Vector3(transform.position.x + xShift, topCenterY, 0), Quaternion.Euler(0, 0, 180), gameObject.transform);
+        TopSpike.GetComponent<RockScript>().isRotated = true;
+    }
 
 
-        // Spawn Fuel or Enemy ----------------------------------------------------------------------------------------------------
+    // SPAWN OBJECTS ----------------------------------------------------------------------------------------------------
+    void SpawnObjects (float centerY)
+    {
+        
         float spwanChance = Random.value;
         float fuelSpawnChange = Mathf.Lerp (0.4f, 0.3f, difficulty);
         float enemySpawnChange = Mathf.Lerp (0.3f, 0.3f, difficulty);
@@ -115,25 +172,25 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnFuel(float y)
     {
-        //float x = Mathf.Lerp (fuelMinXDifference, fuelMaxXDifference, difficulty);
         float shift =  Random.Range(-1, 1);
-        Instantiate(objects[4], new Vector3(transform.position.x + minSpawnDelay, y + shift, 0), Quaternion.identity, gameObject.transform);
+        Instantiate(objects[0], new Vector3(transform.position.x + xShiftingObjects, y + shift, 0), Quaternion.identity, gameObject.transform);
     }
 
     void SpawnEnemy(float y)
     {
-        //float x = Mathf.Lerp (fuelMinXDifference, fuelMaxXDifference, difficulty);
+
         float shift =  Random.Range(-1, 1);
-        Instantiate(objects[5], new Vector3(transform.position.x + minSpawnDelay, y + shift, 0), Quaternion.Euler(0, 0, 0), gameObject.transform);
+        Instantiate(objects[1], new Vector3(transform.position.x + xShiftingObjects, y + shift, 0), Quaternion.Euler(0, 0, 0), gameObject.transform);
     }
 
     void SpawnRocket(float y)
     {
-        //float x = Mathf.Lerp (fuelMinXDifference, fuelMaxXDifference, difficulty);
+
         float shift =  Random.Range(-1, 1);
-        Instantiate(objects[6], new Vector3(transform.position.x + minSpawnDelay, y + shift, 0), Quaternion.Euler(0, 0, 0), gameObject.transform);
+        Instantiate(objects[2], new Vector3(transform.position.x + xShiftingObjects, y + shift, 0), Quaternion.Euler(0, 0, 0), gameObject.transform);
     }
 
+    // REVIVE ----------------------------------------------------------------------------------------------------
     public void ReviveDeleteAllChilds()
     {
         foreach (Transform child in transform)
